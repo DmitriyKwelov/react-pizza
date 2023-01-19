5f68c383bddc948b060a0b38106d8f1a45b9cbad
kwell <74409210+DmitriyKwelov@users.noreply.github.com>
2023-01-19 04:46:21 +0300

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from 'react'
+import './scss/app.scss'
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
 const App = () => {
     return (
         <div>
-          <h1>Hello world!</h1>
+            <div className="wrapper">
+                <Header/>
+                <div className="content">
+                    <div className="container">
+                        <div className="content__top">
+                            <Categories/>
+                            <Sort/>
+                        </div>
+                        <h2 className="content__title">Все пиццы</h2>
+                        <div className="content__items">
+                            <PizzaBlock title="Мексиканская" price={500} />
+                            <PizzaBlock title="hello" price={350} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
